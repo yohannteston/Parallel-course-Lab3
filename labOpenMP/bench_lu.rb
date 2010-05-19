db70da7fe5bc7fb21b8e610b254dbacb130babfe
Matthieu Maury <mayeu.tik@gmail.com>
2010-05-19 02:22:14 +0200

    Add benchmark for lu

---
+++ labOpenMP/bench_lu.rb
@@ -0,0 +1,19 @@
+#!/usr/local/bin/ruby
+
+# Benchmark for gram2
+# For each size and each number of thread launch 20 times
+# the program and keep the least time.
+[1,4,8,12,16,24,32,48,64].each do |n|
+	min_time = 10000000000 ;
+
+	20.times {
+		system "export OMP_NUM_THREAD=4"
+		out = `./lu` 
+		out_split = out.split " " ;
+		if(out_split[1].to_f < min_time)
+			min_time = out_split[1].to_f;
+		end
+	}
+
+	puts "#{size} #{n}  #{min_time}" ;
+end
