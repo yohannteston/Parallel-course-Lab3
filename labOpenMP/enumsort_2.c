9f4a9d995aba1b36486a8d8e17ee8e8081ad0165
yohannteston <yohann.teston@free.fr>
2010-05-19 12:56:06 +0200

    Finish loop

--- labOpenMP/enumsort_2.c
+++ labOpenMP/enumsort_2.c
@@ -9,54 +9,44 @@
 
 int timer(void);
 
-void print(double* a, int size){
-	int i;
-	for(i=0;i<size;i++)
-		printf("%f ",a[i]);
-	printf("\n");
-}
-
 int main(int argc, char *argv[]) {
 
-  int len=50000,seed,i,j,rank;
-  double *indata, *outdata,wtime;
-  
-  indata=(double *)malloc(len*sizeof(double));
-  outdata=(double *)malloc(len*sizeof(double));
+	int len=50000,seed,i,j,rank;
+	double *indata, *outdata,wtime;
 
-  /* Generate random numbers (Wichmann-Hill) */
-  seed=171;
-  for (i=0;i<len;i++){
-    seed=(171*seed)%30269;
-    indata[i]=seed/30268.0;
-    outdata[i]=-1;
-  }
+	indata=(double *)malloc(len*sizeof(double));
+	outdata=(double *)malloc(len*sizeof(double));
 
-  //print(indata,len);
+	/* Generate random numbers (Wichmann-Hill) */
+	seed=171;
+	for (i=0;i<len;i++){
+		seed=(171*seed)%30269;
+		indata[i]=seed/30268.0;
+		outdata[i]=-1;
+	}
 
-  /* Enumeration sort */
-  wtime=omp_get_wtime();
+	/* Enumeration sort */
+	wtime=omp_get_wtime();
 
-  omp_set_nested(1);
+	omp_set_nested(1);
 
 #pragma omp parallel for private(rank,i) 
-  for (j=0;j<len;j++)
-    {
-	
-	rank = 0;
+	for (j=0;j<len;j++)
+	{
+
+		rank = 0;
 #pragma omp parallel for reduction(+:rank) 
-	for (i=0;i<len;i++)
-		if (indata[i]<indata[j]) rank++;
-      outdata[rank]=indata[j];
-    }
-  wtime=omp_get_wtime()-wtime;
-  printf("Time: %f sec \n",wtime);
+		for (i=0;i<len;i++)
+			if (indata[i]<indata[j]) rank++;
+		outdata[rank]=indata[j];
+	}
+	wtime=omp_get_wtime()-wtime;
+	printf("Time: %f sec \n",wtime);
 
-  /* Check results, -1 implies data same as the previous element */
-    for (i=0; i<len-1; i++)
-      if (outdata[i]>outdata[i+1] && outdata[i+1]>-1)
-	printf("ERROR: %f,%f\n", outdata[i],outdata[i+1]);
-    //print(outdata, len);
+	/* Check results, -1 implies data same as the previous element */
+	for (i=0; i<len-1; i++)
+		if (outdata[i]>outdata[i+1] && outdata[i+1]>-1)
+			printf("ERROR: %f,%f\n", outdata[i],outdata[i+1]);
 
-  return 0;
+	return 0;
 }
